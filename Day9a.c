c481b29826c65809db1ffc938c35f63563d08303
Adam <adamhaun@gmail.com>
2016-12-10 21:34:06 -0600

    First draft of Day 9a. Partial Day 9b.

--- Day9a.c
+++ Day9a.c
@@ -17,7 +17,8 @@
 //state and (optionally) changes in the input. State machines are common in
 //digital hardware because they're flexible and easy to implement, but we can
 //also make state machine in software. In C, this is done using a while loop and
-//a switch-case block.
+//a switch-case block. None of this will be super-efficient -- it's more to
+//illustrate the concepts.
 
 
 #include <stdio.h>
@@ -44,13 +45,11 @@ void Create_String(string_t *str, size_t size)
 {
 	//Make sure the buffer is filled with zeros
 	str->used = 0;
-	str->s = calloc(size);
-	if (retVal.s != NULL)
+	str->s = calloc(size, sizeof(char));
+	if (str->s != NULL)
 		str->bufSize = size;
 	else
 		str->bufSize = 0;
-		
-	return retVal;
 }
 	
 //In a real program, this function would never be used by any code outside of
@@ -58,16 +57,18 @@ void Create_String(string_t *str, size_t size)
 //other files, which prevents it from polluting the global namespace.
 static void Enlarge_String(string_t *str)
 {
-	str->s = realloc(str->s, str.bufSize * 2);
+	str->s = realloc(str->s, str->bufSize * 2);
 	
 	if (str->s == NULL)
 	{
-		//Minimal error handling
-		str->bufSize = 0;
-		str->used = 0;
+		//Minimal error handling -- just exit
+		fprintf(stderr, "\nError reallocating string!\n\n");
+		exit(EXIT_FAILURE);
 	} else
 	{
-		//Fill the new section of the string with zeros
+		//Fill the new section of the string with zeros. We just doubled the
+		//size of the string, so our starting offset is half that, which is
+		//equal to the old buffer size. The length is also half that.
 		memset(str->s + str->bufSize, '\0', str->bufSize);
 		
 		//Adjust the buffer size
@@ -76,10 +77,11 @@ static void Enlarge_String(string_t *str)
 }
 
 //Add a character to the end of the string
-void Add_Char(string_t *str, char c)
+void Add_Char_To_String(string_t *str, char c)
 {
-	//Enlarge the string if we've run out of space
-	if (str->used == str->bufSize)
+	//Enlarge the string if we've run out of space. We need to make sure there's
+	//enough space for the new character and a null terminator.
+	if (str->used + 1 + 1 == str->bufSize)
 		Enlarge_String(str);
 		
 	//This could have been one line, but it's better to avoid relying too much
@@ -87,6 +89,29 @@ void Add_Char(string_t *str, char c)
 	//writing code that needs those, you're probably doing something wrong.
 	str->s[str->used] = c;
 	str->used++;
+}
+
+//Concatenate two strings
+void Concatenate_Strings(string_t *dest, const string_t *src)
+{
+	//Allocate more memory as needed. We still need to account for the null
+	//terminator.
+	while (dest->used + 1 + src->used > dest->bufSize)
+	{
+		Enlarge_String(dest);
+	}
+	
+	//We might as well use strcat() to do the copying
+	strcat(dest->s, src->s);
+	dest->used += src->used;
+}
+	
+
+//Clear the string data without deallocating memory
+void Reset_String(string_t *str)
+{
+	memset(str->s, '\0', str->used);
+	str->used = 0;
 }
 
 //Free the memory used by the string
@@ -103,9 +128,9 @@ typedef enum
 {
 	NORMAL_READ,        //Read and (if not special) copy one character
 	GET_MARKER_LENGTH,  //Read the length of the marker
-	GET_MARKER_REPS,    //Read the number of repetitions of the marker
+	GET_MARKER_REPEATS, //Read the number of repetitions of the marker
 	READ_MARKED_CHARS,  //Read the marked characters up to the specified length
-	PRINT_MARKED_CHARS. //Print the marked characters one or more times
+	PRINT_MARKED_CHARS, //Print the marked characters one or more times
 	DONE                //End of input -- exit the loop
 } eState;
 
@@ -114,8 +139,10 @@ int main(int argc, char **argv)
 {
 	//This will be our default state
 	eState state = NORMAL_READ;
+	FILE *inFile;
+	int mark = 0, repeats = 0;
 	int next;
-	string_t output;
+	string_t output, marked;
 	
 	//The usual command line argument check and input file opening
 	if (argc != 2)
@@ -131,12 +158,22 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 	
-	//Create a string buffer. The output is going to be long, so let's start it
-	//at 256 characters.
-	Create_String(&output, 256);
-	if (output->s == NULL)
+	//Create the output buffer. The output is going to be long, so let's start
+	//it at 4096 characters.
+	Create_String(&output, 4096);
+	if (output.s == NULL)
 	{
-		fprintf(stderr, "Error creating string\n\n");
+		fprintf(stderr, "Error creating output string\n\n");
+		return EXIT_FAILURE;
+	}
+	
+	//Create the marked character buffer. Eyeballing the input file, it seems
+	//like 256 is a reasonable starting point.
+	Create_String(&marked, 256);
+	if (marked.s == NULL)
+	{
+		fprintf(stderr, "Error creating marked character string\n\n");
+		Delete_String(&output);
 		return EXIT_FAILURE;
 	}
 
@@ -153,23 +190,79 @@ int main(int argc, char **argv)
 			//it to the output.
 			case NORMAL_READ:
 				if (next == '(')
+				{
+					//Reset the marker variables whenever we reach a new marker
+					mark = 0;
+					repeats = 0;
+					Reset_String(&marked);
 					state = GET_MARKER_LENGTH;
-				else if (next == '\n')
+				} else if (next == '\n')
+				{
 					state = DONE;
-				else
-					Add_Char(&str, (char)next);
+				} else
+				{
+					Add_Char_To_String(&output, (char)next);
+				}
 				//Don't forget the break!
 				break;
-				
+			
+			//Get the number of characters to mark. Move on to the next state
+			//when we encounter an 'x'.
 			case GET_MARKER_LENGTH:
+				if (next == 'x')
+					state = GET_MARKER_REPEATS;
+				else
+					mark = 10*mark + (next - '0');
+				break;
+				
+			//Get the number of times to repeat the marked characters. Move on
+			//to the next state when we encounter a ')'.
+			case GET_MARKER_REPEATS:
+				if (next == ')')
+					state = READ_MARKED_CHARS;
+				else
+					repeats = 10*repeats + (next - '0');
+				break;
 			
+			//Read marked characters until we reach the amount specified
+			case READ_MARKED_CHARS:
+				Add_Char_To_String(&marked, (char)next);
+				mark--;
+				if (mark == 0)
+					state = PRINT_MARKED_CHARS;
+				break;
+				
+			//Copy the marked characters to the output buffer. Repeat the
+			//specified number of times. This is the only state where we don't
+			//process the current character, so we need to put it back before
+			//returning to the normal read state.
+			case PRINT_MARKED_CHARS:
+				while (repeats--)
+				{
+					Concatenate_Strings(&output, &marked);
+				}
+				ungetc(next, inFile);
+				state = NORMAL_READ;
+				break;
 			
+			//It's always a good idea to have a default case
+			default:
+				fprintf(stderr, "Unknown state encountered\n\n");
+				Delete_String(&output);
+				Delete_String(&marked);
+				return EXIT_FAILURE;
 		}
 	}
 	
 	
-	//Close the file as soon as we're done with it
+	//Close the file and delete the marked string as soon as we're done with
+	//them.
+	Delete_String(&marked);
 	fclose(inFile);
+	
+	//Print the number of characters in the output string. Note the special
+	//format specifier for size_t.
+	printf("Number of output characters: %zu\n", output.used);
 		
 	return EXIT_SUCCESS;
 }
